3285f08a10e5d9b7e3d99b5c3084ac3806b0d15b
MatiCobos <91283667+MatiCobos@users.noreply.github.com>
2022-09-29 21:15:19 -0300

    add fetch

--- main.js
+++ main.js
@@ -1,41 +1,28 @@
-const products = [
+/* const products = [
     {id: 1, name: "Home Shirt", price: 100, img: "/public/homeShirt.jpg", description: "This product will be available soon", category: "home shirt", quantity: 1},
     {id: 2, name: "Away Shirt", price: 100, img: "/public/awayShirt.jpg", description: "This product will be available soon", category: "away shirt", quantity: 1},
     {id: 3, name: "Pants", price: 300, img: "/public/sweatPants.jpg", description: "This product will be available soon", category: "pants",  quantity: 1},
     {id: 4, name: "Jacket", price: 400, img: "/public/jacket.jpg", description: "This product will be available soon", category: "jacket",  quantity: 1},
-];
+]; */
 
 const cart = [];
-
-//Vacio el carrito
-const deleteButon = document.getElementById("deleteButon");
-deleteButon.addEventListener("click", ()=>{
-    cart.length = 0;
-    Swal.fire(
-    'SHOPPING CART',
-    'Your cart is empty. If you see something you would like to add to your bag while shopping, click Add to cart',
-    'success'
-)
-    updateCart();
-});
-
 
 //Tratando de guardar en el local storage (no me salió, no entendi)
 /* document.addEventListener("DOMContentLoaded", ()=>{
     if (localStorage.getItem("cart")){
         cart = JSON.parse(localStorage.getItem("cart"))
-        updateCart()
+        renderCart()
     }
 })
  */
 
-
 // Pinto todos los productos en la Home
-const container = document.getElementById("container");
-
-products.forEach((product) => {
+fetch('./data.json')
+.then((resp)=>resp.json())
+.then((data) => {
+    const container = document.getElementById("container");
+    data.forEach((product) => {
     let card = document.createElement("div");
-    /* card.classList.add() me guardo esto para cuando estile asi no me olvido la sintaxis */
     card.innerHTML = `<div class="card" style="width: 18rem;">
                         <img src="${product.img}" class="card-img-top" alt="...">
                         <div class="card-body">
@@ -53,38 +40,43 @@ products.forEach((product) => {
     });
 });
 
+});
+
 //Boton agregar al carrito
 const addToCart = (prodId) => {
     const exist = cart.some (product => product.id === prodId);
     if (exist){
         const prod = cart.map (product => {
             if (product.id === prodId){
-                product.quantity++
+                product.quantity++   
             };
+
         });
     }
-    else{
+
+        //Así lo tenia antes, pero me decia que products is not defined
+/*         else{
         const item = products.find ((product) => product.id === prodId);
         cart.push(item);
         console.log(cart); 
+    }; */
+    else{
+        fetch('./data.json')
+        .then((resp)=>resp.json())  
+        .then((data) => {
+        const item = data.find ((product) => product.id === prodId);
+        cart.push(item);;
+        })
     };
 
-    updateCart();
-}
-
-//Boton borrar item del carrito
-const deleteItem = (prodId)=>{
-    const item = cart.find ((product)=> product.id === prodId);
-    const indice = cart.indexOf(item);
-    cart.splice(indice, 1);
-    updateCart();
+    renderCart();
 };
 
 //Pinto todos los productos en el carrito
 const cartContainer = document.getElementById("cartContainer");
 const totalPrice = document.getElementById("totalPrice");
 
-const updateCart = () =>{
+const renderCart = () =>{
 cartContainer.innerHTML =""
 
     cart.forEach((product)=>{
@@ -102,20 +94,37 @@ cartContainer.innerHTML =""
     totalPrice.innerText = cart.reduce((acc, product) => acc + product.price * product.quantity, 0);
 };
 
+//Boton borrar item del carrito
+const deleteItem = (prodId)=>{
+    const item = cart.find ((product)=> product.id === prodId);
+    const indice = cart.indexOf(item);
+    cart.splice(indice, 1);
+    renderCart();
+};
+
+//Vacio el carrito
+const deleteButton = document.getElementById("deleteButton");
+deleteButton.addEventListener("click", ()=>{
+    cart.length = 0;
+    Swal.fire(
+    'SHOPPING CART',
+    'Your cart is empty. If you see something you would like to add to your bag while shopping, click Add to cart',
+    'success'
+)
+    renderCart();
+});
+
 
 //Barra de busqueda
 let input = document.getElementById("search");
 
 input.addEventListener('keyup', e => {
-    if (e.target.matches("#search")){ //busca el id search
-        //Selecciona todas los productos
-        document.querySelectorAll(".card").forEach(product => { //recorremos los productos
-            //convertimos a minuscula para evitar errores en la busqueda
-            product.textContent.toLowerCase().includes(e.target.value.toLowerCase()) //busca el caracter ingresado
-                //Si esta se quita la clase filtro definida en css
+    if (e.target.matches("#search")){
+        document.querySelectorAll(".card").forEach(product => {
+            product.textContent.toLowerCase().includes(e.target.value.toLowerCase())
                 ?product.classList.remove("filtro")
-                //Si no esta la agrega
             :product.classList.add("filtro")
-        })
-    }
-})
+        });
+    };
+});
+
